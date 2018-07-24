91325d2b629f33587da3bc307c9e3ebba896ee92
Grzegorz Krukiewicz-Gacek <greggscutum@gmail.com>
2018-07-24 13:17:48 +0200

    Updated Examples to use the updated MirrorClient API

---
+++ Examples/ObjectiveC/ObjC/ViewController.m
@@ -0,0 +1,58 @@
+#import "ViewController.h"
+
+@import MirrorDisplay;
+@import EstimoteProximitySDK;
+
+
+@interface ViewController ()
+
+@property (strong, nonatomic) MirrorClient *mirrorClient;
+@property (strong, nonatomic) EPXProximityObserver *proximityObserver;
+
+@end
+
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    Poster *sneakersBanner = [[Poster alloc] init:^(PosterBuilder *p) {
+        
+        p.header = @"Exceptional traction\nfrom your first to final mile";
+        p.image = [Image preloadedImageWithPath:@"shoe_big.jpg"];
+        
+        p.style = [[PosterStyle alloc] init:^(PosterStyleBuilder *ps) {
+            ps.imagePosition =
+            [[Position alloc] initWithHorizontal:[HorizontalPosition center]
+                                        vertical:[VerticalPosition topWithOffset:80]];
+            ps.textPosition =
+            [[Position alloc] initWithHorizontal:[HorizontalPosition center]
+                                        vertical:[VerticalPosition bottomWithOffset:80]];
+            ps.textAlign = TextAlignCenter;
+            ps.headerFontSize = [FontSize fontSizeWithPercent:130];
+        }];
+    }];
+    
+    self.mirrorClient = [[MirrorClient alloc] initWithAppID:@"" appToken:@""];
+
+    EPXCloudCredentials *credentials = [[EPXCloudCredentials alloc] initWithAppID:@"" appToken:@""];
+    
+    self.proximityObserver = [[EPXProximityObserver alloc] initWithCredentials:credentials errorBlock:^(NSError * _Nonnull error) {
+        NSLog(@"%@", error);
+    }];
+    
+    
+    EPXProximityZone *mirrorZone = [[EPXProximityZone alloc] initWithRange:[EPXProximityRange customRangeWithDesiredMeanTriggerDistance:1.0] tag:@"mirror"];
+    
+    mirrorZone.onEnterAction = ^(id<EPXProximityZoneContext> context) {
+        NSLog(@"Enter mirror");
+        [self.mirrorClient displayView:sneakersBanner onMirrorWithIdentifier:context.deviceIdentifier];
+    };
+    mirrorZone.onExitAction = ^(id<EPXProximityZoneContext> context) {
+        NSLog(@"Exit mirror");
+    };
+}
+
+
+@end
---
+++ Examples/Swift/Swift/ViewController.swift
@@ -0,0 +1,43 @@
+import UIKit
+
+import MirrorDisplay
+import EstimoteProximitySDK
+
+class ViewController: UIViewController {
+
+    let mirrorClient = MirrorClient(appID: "", appToken: "")
+    var proximityObserver: EPXProximityObserver!
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let sneakersBanner = Poster() { p in
+            
+            p.header = "Exceptional traction\nfrom your first to final mile"
+            p.image = .preloaded(path: "shoe_big.jpg")
+            
+            p.style = PosterStyle() { ps in
+                ps.imagePosition = Position(horizontal: .center, vertical: .top(offset: 80))
+                ps.textPosition = Position(horizontal: .center, vertical: .bottom(offset: 80))
+                ps.textAlign = .center
+                ps.headerFontSize = .percent(130)
+            }
+        }
+        
+        let credentials = EPXCloudCredentials(appID: "", appToken: "")
+        self.proximityObserver = EPXProximityObserver(credentials: credentials, errorBlock: { error in
+            print("\(error)")
+        })
+        
+        let mirrorZone = EPXProximityZone(range: EPXProximityRange.near,
+                                             tag: "mirror")
+        mirrorZone.onEnterAction = { zoneContext in
+            print("Enter mirror")
+            self.mirrorClient.display(sneakersBanner, onMirror: zoneContext.deviceIdentifier)
+        }
+        mirrorZone.onExitAction = { zoneContext in
+            print("Exit mirror")
+        }
+    }
+}
